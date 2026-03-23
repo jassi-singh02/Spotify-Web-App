a8c7c37297e9064afa2a9ee826dafacf217ece14
Jaskaran Singh <jsingh21002@gmail.com>
2026-03-22 22:58:45 -0400

    more deployment stuff

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.set('trust proxy', 1);
 // check in 
 // Serve static assets and parse form bodies
 app.use(express.static("public"));
